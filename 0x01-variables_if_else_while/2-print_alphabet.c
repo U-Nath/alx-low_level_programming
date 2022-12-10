8760bc102b847a6f4bf0f72b442fe3ca0635e899
U-Nath <umaraubali36@gmail.com>
2022-12-10 09:41:17 -0800

    alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description 'Lists the letters of the alphabet'
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	int letter;
+
+	letter = 97;
+	while (letter <= 122)
+	{
+		putchar(letter);
+		letter++;
+	}
+	printf("\n");
+	return (0);
+}
