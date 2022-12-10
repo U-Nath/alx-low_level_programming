b4d48b241304673a489b84045407d16e98a98d48
U-Nath <umaraubali36@gmail.com>
2022-12-10 10:01:50 -0800

    alphABET game

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description 'Lists the letters of the alphabet'
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	int letter;
+
+	letter = 97;
+	while (letter <= 122)
+	{
+		putchar(letter);
+		letter++;
+	}
+
+	letter = 65;
+	while (letter <= 90)
+	{
+		putchar(letter);
+		letter++;
+	}
+	printf("\n");
+	return (0);
+}
