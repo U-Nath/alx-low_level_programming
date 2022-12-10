c384b88734815e46619cd3071823515ce7297442
U-Nath <umaraubali36@gmail.com>
2022-12-10 12:02:18 -0800

    numbers game

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description 'Lists the base 10 digits'
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	int number;
+
+	number = 48;
+	while (number <= 57)
+	{
+		putchar(number);
+		number++;
+	}
+	printf("\n");
+	return (0);
+}
