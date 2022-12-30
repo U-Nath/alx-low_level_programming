7994a666d9e511722e8cd639ed6b5e4056589ddf
U-Nath <umaraubali36@gmail.com>
2022-12-30 13:52:01 -0800

    listing numbers upto 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,12 +9,26 @@
 
 void print_to_98(int n)
 {
-	while (n <= 98)
-	{
-		printf("%d", n);
-		if (n < 98)
-			printf(", ");
-		n++;
+	if (n <= 98)
+	{		
+		while (n <= 98)
+		{
+			printf("%d", n);
+			if (n < 98)
+				printf(", ");
+			n++;
+		}
+		printf("\n");
 	}
-	printf("\n");
+	else
+	{
+		while (n >= 98)
+		{
+			printf("%d", n);
+			if (n > 98)
+				printf(", ");
+			n--;
+		}
+		printf("\n");
+	}
 }
