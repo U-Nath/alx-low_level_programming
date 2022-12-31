f2ec3673d1055fa5a37b58442b90b52b2c15b4fc
U-Nath <umaraubali36@gmail.com>
2022-12-31 11:26:51 -0800

    Fibonacci 3

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -11,17 +11,43 @@ void fibonacci3(void)
 	int n;
 	unsigned long a_x;
 	unsigned long a_y;
+	unsigned long half_a_x, half_a_y, halfa_x, halfa_y;
+	unsigned long half_, half;
 
 	a_x = 1;
 	a_y = 2;
 	n = 1;
-	while (n <= 98)
+	while (n <= 92)
 	{
 		printf("%lu", a_x);
 		a_y = a_x + a_y;
 		a_x = a_y - a_x;
 		if (n < 98)
 			printf(", ");
+		n++;
+	}
+
+	half_a_x = a_x / 10000000000;
+	half_a_y = a_y / 10000000000;
+	halfa_x = a_x % 10000000000;
+	halfa_y = a_y % 10000000000;
+
+	while (n >= 93 && n <= 98)
+	{
+		half_ = half_a_x + half_a_y;
+		half = halfa_x + halfa_y;
+		if ((half_ + half) > 9999999999)
+		{
+			half_ += 1;
+			half %= 10000000000;
+		}
+		printf("%lu%lu", half_, half);
+		if (n < 98)
+			printf(", ");
+		half_a_x = half_a_y;
+		halfa_x = halfa_y;
+		half_a_y = half_;
+		halfa_y = half;
 		n++;
 	}
 	printf("\n");
