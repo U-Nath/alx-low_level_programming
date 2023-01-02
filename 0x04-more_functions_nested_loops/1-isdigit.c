09899fecd45a4399599985962fffce6d7900fbdb
U-Nath <umaraubali36@gmail.com>
2023-01-02 12:27:13 -0800

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * _isdigit - checks whether the chars are numeric
+ * @c: the character under test
+ * Return: 1 if lower case, 0 if else.
+ */
+
+int _isupper(int c)
+{
+	if (c >= 48 && c <= 58)
+		return (1);
+	else
+		return (0);
+}
