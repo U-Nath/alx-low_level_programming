e47f4fb10808e1efeb4eb6ba3ce7415232f25604
U-Nath <umaraubali36@gmail.com>
2023-01-02 13:38:24 -0800

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * more_numbers - prints 10 times number from 0 to 14
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int num;
+	int counter;
+	
+	counter = 1;
+	while (counter <= 10)
+	{
+		num = 0;
+		while (num <= 14)
+		{
+			printf("%d", num);
+			num++;
+		}
+	printf("\n")
+	}
+}
+
