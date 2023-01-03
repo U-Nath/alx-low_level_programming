93282a95e3d057a81d7c199a106281f3a2545b0c
U-Nath <umaraubali36@gmail.com>
2023-01-03 11:47:28 -0800

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include <stdio.c>
+
+/**
+ * _strlen - states the length of a string
+ *
+ * @s: the string
+ * Return: the length of the string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (*(s + i) != '\0')
+		i++;
+	return(i);
+}
