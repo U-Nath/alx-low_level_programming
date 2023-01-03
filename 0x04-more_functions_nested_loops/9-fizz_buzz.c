90b4ce5efa2890f23af17b252c54834f00826acb
U-Nath <umaraubali36@gmail.com>
2023-01-03 03:04:00 -0800

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,11 +15,11 @@ void fizz_buzz(int limit)
 	while (n <= limit)
 	{
 		if ((n % 3 == 0) && (n % 15 != 0))
-			printf("fizz");
+			printf("Fizz");
 		else if ((n % 5 == 0) && (n % 15 != 0))
-			printf("buzz");
+			printf("Buzz");
 		else if (n % 15 == 0)
-			printf("fizzbuzz");
+			printf("FizzBuzz");
 		else
 			printf("%d", n);
 		if (n < limit)
