c254e36d51e2c03b208ec323960df535cc00ee7e
U-Nath <umaraubali36@gmail.com>
2023-01-03 12:10:31 -0800

    print in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse order of its chars
+ * @s: the string
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	i = 0;
+	while (*(s + i) != '\0')
+		i++;
+
+	for (i >= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
