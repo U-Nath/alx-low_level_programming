7c3942ed5e741f4f086f31a133de8d12d05bc931
U-Nath <umaraubali36@gmail.com>
2023-01-03 11:14:36 -0800

    my first pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - resets a variable to 98 through a pointer
+ *
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+My grand quick journey through pointers arrays strings
