af89a489eef086461913008339df98bfff4f1966
U-Nath <umaraubali36@gmail.com>
2023-01-06 10:39:31 -0800

    concatenate

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,7 +9,8 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int count = 0, scrcount = 0;
+	int count = 0;
+	int scrcount = 0;
 	char *whole;
 
 	while (*(dest + count) != '\0')
@@ -21,7 +22,7 @@ char *_strcat(char *dest, char *src)
 	{
 		*(whole + count) = *(scr + scrcount);
 		scrcount++;
-		count;
+		count++;
 	}
 	return (whole);
 }
