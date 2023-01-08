84b1ad0c8ff10c4504674d1405d879f6a6ed65f0
U-Nath <umaraubali36@gmail.com>
2023-01-08 02:19:53 -0800

    Capitalize

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalize each word in the string
+ * @str: thr given string
+ *
+ * Return: thr capitalized string
+ */
+
+char *cap_string(char *str)
+{
+	int count;
+
+	count = 0;
+	while (*(str + count) != '\0')
+	{
+		if (*(str + count) >= 97 && *(str + count) <= 122 && *(str + count - 1) == ' ')
+			*(str + count) -= 32;
+		count++;
+	}
+	return (str);
+}
