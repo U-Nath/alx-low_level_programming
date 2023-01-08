701ced4c259bebe29acd180ac341e63932b25159
U-Nath <umaraubali36@gmail.com>
2023-01-08 06:26:38 -0800

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * leet - rewrites in number form
+ * @str: the given string
+ *
+ * Return: the enrypted string
+ */
+
+char *leet(char *str)
+{
+	int a, b;
+	char *upper = {AEOTL};
+	char *lower = "aeotl";
+	char *number = "43071";
+
+	a = 0;
+	while (*(str + a) != '/0')
+	{
+		b = 0;
+		while (*(upper + b) != '\0' || *(lower + b) != '\0')
+		{
+			if (str[a] == upper[b] || str[a] == lower[b])
+				str[a] = number[b];
+		}
+	}
+	return (str);
+}
