1de1fa29c017c6a561855479befc0b5b9e9893bc
U-Nath <umaraubali36@gmail.com>
2023-01-14 04:25:19 -0800

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * _memset - sets the buffer to some value
+ * @s: the buffer to be adjusted
+ * @b: the character which will fill the memory
+ * @n: the number of bytes consumed
+ * Return: the adjusted buffer
+ */
+
+char *_memset(char *s, char b, int n)
+{
+	int count;
+
+	for (count = 0; count <= n; count++)
+		*(s + count) = b;
+	return (*s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+my journey through arrays and pointers continues in this file into its last part, at least for now.
