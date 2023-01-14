48ba4e8df3f0af3fa8797cc76bd985c6f7c73b97
U-Nath <umaraubali36@gmail.com>
2023-01-14 06:07:35 -0800

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * _strchr - find a certain char and resetting the string to start
+ * from that char
+ *
+ * @s: the given string
+ * @c: the required character
+ * Return: NULL if the character is not found
+ * if it is found, the function returns the reset string
+ */
+
+char *_strchr(char *s, char c)
+{
+	int count;
+
+	while (*(s + count) != '\0')
+	{
+		if (*(s + count) == c)
+		{
+			s = s + count;
+			return (s);
+		}
+		else
+			count++;
+	}
+	return (NULL);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,5 +3,6 @@
 
 char *_memset(char *s, char b, int n);
 char *_memcpy(char *dest, char *src, int n);
+char *_strchr(char *s, char c);
 
 #endif
