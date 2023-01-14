03f6a8ba4e9efd2b7bf0038f96ad2ebe3c2157a4
U-Nath <umaraubali36@gmail.com>
2023-01-14 05:13:49 -0800

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * _memcpy - adjusts only some elements
+ * @dest: the destination file
+ * @src: the source file
+ * @n: the number of bytes copied
+ */
+
+char *_memcpy(char *dest, char *src, int n)
+{
+	int count;
+
+	for (count = 0; count < n; count++)
+	{
+		dest[count] = src[count];
+	}
+	return(dest);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,5 +2,6 @@
 #define MAIN_H
 
 char *_memset(char *s, char b, int n);
+char *_memcpy(char *dest, char *src, int n);
 
 #endif
