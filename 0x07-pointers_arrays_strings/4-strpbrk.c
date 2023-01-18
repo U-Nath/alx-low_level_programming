efc2b5c28088a8a48552bce5c1a2ea2d7ef5e866
U-Nath <umaraubali36@gmail.com>
2023-01-18 12:06:07 -0800

    40-strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- * _strbrk - searches for a similat character between s and accept and 
+ * _strpbrk - searches for a similat character between s and accept and 
  * prints starting from the similar character
  * @s: the first string
  * @accept: the reference string
  * Return: the adjusted string
  */
 
-char *_strbrk(char *s, char *accept)
+char *_strpbrk(char *s, char *accept)
 {
 	int a, b, i;
 	unsigned int count;
@@ -26,13 +26,13 @@ char *_strbrk(char *s, char *accept)
 				i++;
 				break;
 			}
-			count++;
 			b++;
 		}
 		a++;
 		if (i != 0)
 			break;
+		count++;
 	}
-	*s = *(s + count);
+	s = (s + count);
 	return (s);
 }
