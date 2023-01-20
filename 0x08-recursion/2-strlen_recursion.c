647d5cd804de27a2c33234b301675273e889f76a
U-Nath <umaraubali36@gmail.com>
2023-01-20 12:48:39 -0800

    strlen with recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - counts the chars in a string
+ *
+ * @s: the string
+ * Return: the number of chars
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s)
+		return (1 + _strlen_recursion(s + 1));
+	else
+		return (0);
+}
