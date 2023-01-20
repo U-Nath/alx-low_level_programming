c3ac2373d1671a2eff6adb1955b8a06ec70349a7
U-Nath <umaraubali36@gmail.com>
2023-01-20 14:07:49 -0800

    sqrt recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * preliminary - scans for a sqrt of the number
+ *
+ * @n: the given number
+ * @i: the possible sqrt of the number
+ *
+ * Return: i if exists, otherwise -1
+ */
+int preliminary(int i, int n)
+{
+	if (i * i < n)
+		return (preliminary(i + 1, n));
+	else if (i * i == n)
+		return (i);
+	else
+		return (-1);
+}
+
+/**
+ * _sqrt_recursion - finds the natural sqrt of a number
+ *
+ * @n: the given number
+ * Return: the sqrt of the number
+ * if n < 0 or if not a perfect square, return -1
+ */
+
+int _sqrt_recursion(int n)
+{
+	int i;
+
+	i = 0;
+	if (n < 0)
+		return (-1);
+	else
+		return (evaluate(i, n));
+}
