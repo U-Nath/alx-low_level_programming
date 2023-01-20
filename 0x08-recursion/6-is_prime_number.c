d45c59fbc2020c89eb70593ff6d5f146f76bb56b
U-Nath <umaraubali36@gmail.com>
2023-01-20 14:37:11 -0800

    prime number identify

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -15,7 +15,7 @@ int preliminary(int i, int n)
 	else if (i == n)
 		return (1);
 	else
-		return (-1);
+		return (0);
 }
 
 /**
@@ -32,7 +32,7 @@ int is_prime_number(int n)
 
 	i = 2;
 	if (n <= 1)
-		return (-1);
+		return (0);
 	else
 		return (preliminary(i, n));
 }
