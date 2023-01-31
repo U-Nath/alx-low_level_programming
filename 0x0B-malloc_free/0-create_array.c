7b8e6ca453420f3c663bed3b6ee9e33ca84aec83
U-Nath <umaraubali36@gmail.com>
2023-01-31 09:20:50 -0800

    create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,7 @@ char *create_array(unsigned int size, char c)
 	char *array = malloc(size);
 	unsigned int i;
 
-	array = malloc(sizeof (char) * ize);
+	array = malloc(sizeof (char) * size);
 	if (size == 0 || array == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
