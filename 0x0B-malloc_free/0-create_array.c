680dd15d6bb4a8b6335ae01ae01df2b492d77ade
U-Nath <umaraubali36@gmail.com>
2023-01-31 08:52:42 -0800

    create array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * create_array - creates an array starting with an element
+ * @size: the size of the array
+ * @c: the first element of the array
+ * Return: a pointer to the array
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *array = malloc(size);
+	unsigned int i;
+
+	str = malloc(size);
+	if (size == 0 || array == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		array[i] = c;
+
+	return (array);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+This is how I learned first about heap memory, and about malloc and free
