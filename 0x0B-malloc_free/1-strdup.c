8347671636cdefbf31fa2b34629d47bd8b5f28ea
U-Nath <umaraubali36@gmail.com>
2023-01-31 09:52:44 -0800

    strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - duplicates the string
+ *
+ * @str: the string to be duplicated
+ * Return: the copy of the string
+ */
+
+char *_strdup(char *str)
+{
+	int i;
+	char *array;
+
+	i = 0;
+	while (str[i])
+		i++;
+	if (str == NULL)
+		return (NULL);
+
+	array = malloc(sizeof(char) * i);
+	for (j = 0; j <= i; j++)
+		array[j] = str[j];
+
+	return (array);
+	free(array);
+}
