d365d84f75b5f752fd6cdaba0f99082c1269671a
U-Nath <umaraubali36@gmail.com>
2023-02-02 00:27:20 -0800

    concatenate

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,27 +11,26 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	int i;
+	int i = 0, l = 0;
 	char *array;
-	int j, k;
+	int j = 0, k = 0;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	i = 0;
+
 	while (s1[i])
 		i++;
-	j = 0;
 	while (s2[j])
 		j++;
-	
-	array = malloc(sizeof(char) * (i + j) + 1);
+
+	l = i + j;
+	array = malloc((sizeof(char) * l) + 1);
 	if (array == NULL)
 		return (NULL);
 	j = 0;
-	k = 0;
-	while (k < (i + j))
+	while (k < l)
 	{
 		if (k <= i)
 			array[k] = s1[k];
