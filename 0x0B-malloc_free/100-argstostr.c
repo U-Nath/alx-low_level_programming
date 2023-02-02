20d1104e0a8613a8146a285c9f4655046d72c9c9
U-Nath <umaraubali36@gmail.com>
2023-02-02 07:31:16 -0800

    concatenate arguments

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+char *argstostr(int ac, char **av)
+{
+int i = 0, j = 0, k = 0, ch = 0;
+char *s;
+		    
+if (ac == 0 || av == NULL)
+return (NULL);
+		        
+while(av[i])
+{
+while (av[i][j])
+{
+ch++;
+j++;
+}
+j = 0;
+i++;
+}
+			    
+s = malloc((sizeof(char) * ch) + ac + 1);
+i = 0;
+				    
+while (av[i])
+{
+while (av[i][j])
+{
+s[k] = av[i][j];
+k++;
+j++;
+}
+s[k] = '\n';
+j = 0;
+i++;
+k++;
+}
+s[k] = '\0';
+return (s);
+}
