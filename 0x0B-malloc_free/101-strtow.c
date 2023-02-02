8f27f67707cbd544893c32a9beacfb54c924cef8
U-Nath <umaraubali36@gmail.com>
2023-02-02 10:55:29 -0800

    strtow

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -58,10 +58,12 @@ char **strtow(char *str)
 
 	if (str == NULL || str[0] == 0)
 		return (NULL);
+	words = count_words(str);
+
 	if (words == 0)
 		return (NULL);
 
-	strings = malloc((sizeof(char *) * (words + 1));
+	strings = malloc(sizeof(char *) * (words + 1));
 	if (strings == NULL)
 		return (NULL);
 
